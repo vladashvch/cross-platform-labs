8b9c9f5f05ab50f808bf596bb9e4fc792e2c60ea
Vlada Shevchenko <vlada.shevchenko.it@gmail.com>
2024-10-04 02:18:09 +0300

    Created Lab03

---
+++ Lab03/FilesHandler.cs
@@ -0,0 +1,50 @@
+﻿namespace Lab03
+{
+    public class FilesHandler
+    {
+        private readonly string _inputFilePath;
+        private readonly string _outputFilePath;
+
+        public FilesHandler()
+        {
+            string labFolder = "Lab03";
+            string baseDirectory = Directory.GetCurrentDirectory();
+            string filesFolder = "files";
+
+            if (baseDirectory.EndsWith(labFolder))
+            {
+                _inputFilePath = Path.Combine(baseDirectory, filesFolder, "INPUT.TXT");
+                _outputFilePath = Path.Combine(baseDirectory, filesFolder, "OUTPUT.TXT");
+            }
+            else
+            {
+                _inputFilePath = Path.Combine(baseDirectory, labFolder, filesFolder, "INPUT.TXT");
+                _outputFilePath = Path.Combine(baseDirectory, labFolder, filesFolder, "OUTPUT.TXT");
+            }
+            
+        }
+
+        public string InputFilePath
+        {
+            get { return _inputFilePath; }
+        }
+
+        public string OutputFilePath
+        {
+            get { return _outputFilePath; }
+        }
+
+        public List<string> ProcessInputFile(string inputFilePath)
+        {
+            List<string> lines = new List<string>();
+            string[] fileLines = File.ReadAllLines(inputFilePath);
+
+            foreach (string line in fileLines)
+            {
+                lines.Add(line);
+            }
+
+            return lines;
+        }
+    }
+}
---
+++ Lab03/Program.cs
@@ -0,0 +1,44 @@
+﻿using VShevchenko;
+
+namespace Lab03
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Lab-03");
+                Console.WriteLine();
+                ExecuteProgram();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+
+        public static void ExecuteProgram()
+        {
+            var filesHandler = new FilesHandler();
+
+            string inputFilePath = filesHandler.InputFilePath;
+            string outputFilePath = filesHandler.OutputFilePath;
+
+            List<string> data = filesHandler.ProcessInputFile(inputFilePath);
+            Console.WriteLine("Data from file:");
+            Console.WriteLine(string.Join(Environment.NewLine, data));
+
+            var checker = new ProcedureChecker();
+            checker.ProcessProcedures(data);
+
+            List<string> results = checker.GetResults();
+            Console.WriteLine("Result:");
+            Console.WriteLine(string.Join(Environment.NewLine, results));
+
+            
+            File.WriteAllLines(outputFilePath, results);
+        }
+    }
+}
